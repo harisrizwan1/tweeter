ab6dd91357233e81d0bd3f5a7eed4984bfa7ef4d
harisrizwan1 <harisrizwan985@gmail.com>
2021-12-15 19:35:27 -0600

    implement better error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,62 +1,79 @@
+// generates tweet element
+const createTweetElement = function(data) {
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <img src=${data.user.avatars} />
+        <p>${data.user.name}</p>
+        <p>${data.user.handle}</p>
+      </header>
+      <p>${escape(data.content.text)}</p>
+      <footer>
+        <hr>
+        <section>
+          <time>${timeago.format(data.created_at)}</time>
+          <div>
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </seciton>
+      </footer>
+    </article>
+  `);
+  return $tweet;
+};
+
+// iterates over tweets in database and renders them
+const renderTweets = function(tweets) {
+  const $container = $("#tweets-container");
+  for (const tweet of tweets) {
+    const $createdTweet = createTweetElement(tweet);
+    $container.prepend($createdTweet);
+  }
+};
+
+// loads tweets using ajax get request
+const loadTweets = function() {
+  $.ajax('http://localhost:8080/tweets', {method: "GET"})
+    .then(function(data) {
+      renderTweets(data);
+    });
+};
+
+// escape function
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 $(document).ready(function() {
-  const createTweetElement = function(data) {
-    const $tweet = $(`
-      <article class="tweet">
-        <header>
-          <img src=${data.user.avatars} />
-          <p>${data.user.name}</p>
-          <p>${data.user.handle}</p>
-        </header>
-        <p>${data.content.text}</p>
-        <footer>
-          <hr>
-          <section>
-            <time>${timeago.format(data.created_at)}</time>
-            <div>
-              <i class="fas fa-flag"></i>
-              <i class="fas fa-retweet"></i>
-              <i class="fas fa-heart"></i>
-            </div>
-          </seciton>
-        </footer>
-      </article>
-    `);
-  
-    return $tweet;
-  };
-  
-  const renderTweets = function(tweets) {
-    const $container = $("#tweets-container");
-    for (const tweet of tweets) {
-      const $createdTweet = createTweetElement(tweet);
-      $container.prepend($createdTweet);
-    }
-  };
-
-  const loadTweets = function() {
-    $.ajax('http://localhost:8080/tweets', {method: "GET"})
-      .then(function(data) {
-        renderTweets(data);
-      });
-  };
-
+  // calls loadtweets funtion
   loadTweets();
 
+  // request handler for new tweet submission
   $('#new-tweet-form').submit(function(event) {
     event.preventDefault();
+    const $error = $(this).children('.error');
+    $error.slideUp().empty();
     const text = $(this).children('#tweet-text').val();
+
+    // check if input is valid
     if (!text) {
-      alert("Tweet cannot be blank");
+      $error.append("Tweets cannot be blank").slideDown();
       return false;
     } else if (text.length > 140) {
-      alert("Tweet is too long!");
+      $error.append("Tweet is too long!").slideDown();
       return false;
     }
+
     $.ajax({
       method: "POST",
       url: "http://localhost:8080/tweets/",
       data: $(this).serialize(),
       success: function() {
+        // reload content without refreshing page
         $("#tweets-container").empty();
         loadTweets();
       }
